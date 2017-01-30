661e0dcfbdb36d615d3cdd4ad390821dc21f3769
amrith.sumithra <amrith.sumithra@rakuten.com>
2017-01-30 11:10:09 +0530

    name change

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,17 +1,15 @@
 <?php
 class Api_Controller extends CI_Controller {
-	
-	public $data = array();
-	
-	public function __construct()
-	{
-		parent::__construct();
-		$this->data['status'] = 200;
-		$this->data['message'] = '';
-		$this->data['error'] = '';
+	public $data = array ();
+	public function __construct() {
+		parent::__construct ();
+		$this->data ['status'] = 200;
+		$this->data ['message'] = '';
+		$this->data ['error'] = '';
 	}
 	/**
 	 * function to get the input parameters
+	 * 
 	 * @return array
 	 */
 	public function getJsonInputs() {
@@ -21,8 +19,7 @@ class Api_Controller extends CI_Controller {
 		}
 		return json_decode ( $input, true );
 	}
-	
-	public  function response($data, $statusCode = 200, $contentType = 'application/json') {
+	public function response($data, $statusCode = 200, $contentType = 'application/json') {
 		return $this->output->set_content_type ( $contentType )->set_status_header ( $statusCode )->set_output ( json_encode ( $data ) );
 	}
 }
